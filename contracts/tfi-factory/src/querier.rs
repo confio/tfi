3d485b0c3ddad4bb7df76dcb5e945b8f7098fee9
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-06-23 15:03:47 +0200

    Update tfi-factory

--- contracts/tfi-factory/src/querier.rs
+++ contracts/tfi-factory/src/querier.rs
@@ -1,12 +1,12 @@
 use cosmwasm_std::{Addr, Binary, Deps, QueryRequest, StdResult, WasmQuery};
-use tfi::asset::PairInfoRaw;
+use tfi::asset::PairInfo;
 
 pub fn query_liquidity_token(deps: Deps, contract_addr: Addr) -> StdResult<Addr> {
     // load pair_info form the pair contract
-    let pair_info: PairInfoRaw = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
+    let pair_info: PairInfo = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
         contract_addr: contract_addr.to_string(),
-        key: Binary::from("\u{0}\u{9}pair_info".as_bytes()),
+        key: Binary::from("pair_info".as_bytes()),
     }))?;
 
-    deps.api.addr_humanize(&pair_info.liquidity_token)
+    Ok(pair_info.liquidity_token)
 }
