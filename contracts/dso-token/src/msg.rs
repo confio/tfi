ebe691f3fb9bdd5bfdc07b93d6f9876fbf3b8c3a
Mauro Lacy <mauro@lacy.com.es>
2021-07-16 12:27:17 +0200

    Copy from cw20-whitelist
    Rename to dso-token

---
+++ contracts/dso-token/src/msg.rs
@@ -0,0 +1,58 @@
+use cw20::{Cw20Coin, MinterResponse};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub initial_balances: Vec<Cw20Coin>,
+    pub mint: Option<MinterResponse>,
+    /// This is the address of a cw4 compatible contract that will serve as a whitelist
+    pub whitelist_group: String,
+}
+
+pub type ExecuteMsg = cw20_base::msg::ExecuteMsg;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    /// Returns the cw4 contract used to whitelist this token.
+    /// Return type: WhitelistResponse
+    Whitelist {},
+    /// Returns the current balance of the given address, 0 if unset.
+    /// Return type: BalanceResponse.
+    Balance { address: String },
+    /// Returns metadata on the contract - name, decimals, supply, etc.
+    /// Return type: TokenInfoResponse.
+    TokenInfo {},
+    /// Only with "mintable" extension.
+    /// Returns who can mint and how much.
+    /// Return type: MinterResponse.
+    Minter {},
+    /// Only with "allowance" extension.
+    /// Returns how much spender can use from owner account, 0 if unset.
+    /// Return type: AllowanceResponse.
+    Allowance { owner: String, spender: String },
+    /// Only with "enumerable" extension (and "allowances")
+    /// Returns all allowances this owner has approved. Supports pagination.
+    /// Return type: AllAllowancesResponse.
+    AllAllowances {
+        owner: String,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+    /// Only with "enumerable" extension
+    /// Returns all accounts that have balances. Supports pagination.
+    /// Return type: AllAccountsResponse.
+    AllAccounts {
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct WhitelistResponse {
+    pub address: String,
+}
