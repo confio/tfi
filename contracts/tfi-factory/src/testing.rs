bab13f26d5f2e877107cb5871119281ecdd428e8
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-07-19 17:49:30 +0200

    tfi-factory: non-empty label when instantiating tfi-pair

--- contracts/tfi-factory/src/testing.rs
+++ contracts/tfi-factory/src/testing.rs
@@ -148,7 +148,7 @@ fn create_pair() {
                 .unwrap(),
                 code_id: 321u64,
                 funds: vec![],
-                label: "".to_string(),
+                label: "asset0000-asset0001".to_string(),
                 admin: None,
             }
             .into()
