1735bab6850b681a7bf5586d2f91b06812398124
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-07-30 17:10:04 +0200

    Updated tfi-pair and started on working multitest

--- contracts/tfi-pair/src/multitest.rs
+++ contracts/tfi-pair/src/multitest.rs
@@ -1,7 +1,7 @@
 use cosmwasm_std::testing::{mock_env, MockApi, MockStorage};
 use cosmwasm_std::{coins, Addr, Empty, StdError, Uint128};
 use cw20::{Cw20Coin, Cw20ExecuteMsg};
-use cw_multi_test::{App, Contract, ContractWrapper, SimpleBank};
+use cw_multi_test::{App, BankKeeper, Contract, ContractWrapper, Executor};
 
 use crate::error::ContractError;
 use tfi::asset::{Asset, AssetInfo};
@@ -10,18 +10,18 @@ use tfi::pair::{ExecuteMsg, InstantiateMsg, QueryMsg, SimulationResponse};
 fn mock_app() -> App {
     let env = mock_env();
     let api = Box::new(MockApi::default());
-    let bank = SimpleBank {};
+    let bank = BankKeeper::new();
 
-    App::new(api, env.block, bank, || Box::new(MockStorage::new()))
+    App::new(api, env.block, bank, Box::new(MockStorage::new()))
 }
 
 pub fn contract_pair() -> Box<dyn Contract<Empty>> {
-    let contract = ContractWrapper::new_with_reply(
+    let contract = ContractWrapper::new(
         crate::contract::execute,
         crate::contract::instantiate,
         crate::contract::query,
-        crate::contract::reply,
-    );
+    )
+    .with_reply(crate::contract::reply);
     Box::new(contract)
 }
 
@@ -42,7 +42,7 @@ fn setup_liquidity_pool() {
     // set personal balance
     let owner = Addr::unchecked("owner");
     let init_funds = coins(20000, "btc");
-    app.set_bank_balance(&owner, init_funds).unwrap();
+    app.init_bank_balance(&owner, init_funds).unwrap();
 
     // set up cw20 contract with some tokens
     let cw20_id = app.store_code(contract_cw20());
@@ -136,42 +136,14 @@ fn setup_liquidity_pool() {
         ],
         slippage_tolerance: None,
     };
-    // This is failing due to multitest limitations in 0.6
     let _ = app
-        .execute_contract(owner, pair_addr, &msg, &coins(10, "btc"))
-        .unwrap_err();
+        .execute_contract(owner, pair_addr.clone(), &msg, &coins(10, "btc"))
+        .unwrap();
 
-    // // simulate again
-    // let res: SimulationResponse = app.wrap().query_wasm_smart(&pair_addr, &query_msg).unwrap();
-    // // doubling the amount of cash should return half the BTC from the LP
-    // assert_eq!(res.return_amount, Uint128::new(5));
+    // simulate again
+    let res: SimulationResponse = app.wrap().query_wasm_smart(&pair_addr, &query_msg).unwrap();
+    // doubling the amount of cash should return half the BTC from the LP
+    assert_eq!(res.return_amount, Uint128::new(5));
 
-    // // send some tokens to create an escrow
-    // let arb = Addr::unchecked("arbiter");
-    // let ben = String::from("beneficiary");
-    // let id = "demo".to_string();
-    // let create_msg = ReceiveMsg::Create(CreateMsg {
-    //     id: id.clone(),
-    //     arbiter: arb.to_string(),
-    //     recipient: ben.clone(),
-    //     end_height: None,
-    //     end_time: None,
-    //     cw20_whitelist: None,
-    // });
-    // let send_msg = Cw20ExecuteMsg::Send {
-    //     contract: escrow_addr.to_string(),
-    //     amount: Uint128::new(1200),
-    //     msg: to_binary(&create_msg).unwrap(),
-    // };
-    // let res = router
-    //     .execute_contract(owner.clone(), cash_addr.clone(), &send_msg, &[])
-    //     .unwrap();
-    // assert_eq!(2, res.events.len());
-    // println!("{:?}", res.events);
-    // let cw20_attr = res.custom_attrs(0);
-    // println!("{:?}", cw20_attr);
-    // assert_eq!(4, cw20_attr.len());
-    // let escrow_attr = res.custom_attrs(1);
-    // println!("{:?}", escrow_attr);
-    // assert_eq!(2, escrow_attr.len());
+    // TODO: actually perform swap and check value
 }
