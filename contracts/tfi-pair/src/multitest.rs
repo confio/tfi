9a65e6b9f594d5fe43ea6dcc3af9c86ca80d15ec
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-08-06 12:45:22 +0200

    Basic full swap multitest scenario

--- contracts/tfi-pair/src/multitest.rs
+++ contracts/tfi-pair/src/multitest.rs
@@ -1,18 +1,19 @@
 use cosmwasm_std::testing::{mock_env, MockApi, MockStorage};
-use cosmwasm_std::{coins, Addr, Empty, StdError, Uint128};
+use cosmwasm_std::{coin, coins, to_binary, Addr, Empty, StdError, Uint128};
 use cw20::{Cw20Coin, Cw20ExecuteMsg};
 use cw_multi_test::{App, BankKeeper, Contract, ContractWrapper, Executor};
 
 use crate::error::ContractError;
-use tfi::asset::{Asset, AssetInfo};
-use tfi::pair::{ExecuteMsg, InstantiateMsg, QueryMsg, SimulationResponse};
+use tfi::asset::{Asset, AssetInfo, PairInfo};
+use tfi::pair::{Cw20HookMsg, ExecuteMsg, InstantiateMsg, QueryMsg, SimulationResponse};
 
 fn mock_app() -> App {
     let env = mock_env();
     let api = Box::new(MockApi::default());
     let bank = BankKeeper::new();
+    let storage = Box::new(MockStorage::new());
 
-    App::new(api, env.block, bank, Box::new(MockStorage::new()))
+    App::new(api, env.block, bank, storage)
 }
 
 pub fn contract_pair() -> Box<dyn Contract<Empty>> {
@@ -32,6 +33,75 @@ pub fn contract_cw20() -> Box<dyn Contract<Empty>> {
         cw20_base::contract::query,
     );
     Box::new(contract)
+}
+
+#[track_caller]
+// Checks if allowances on cw20 contracts are as expected
+fn assert_allowances(app: &App, addr: Addr, owner: Addr, allowances: Vec<cw20::AllowanceInfo>) {
+    let nonzero_allow = |allow: &cw20::AllowanceInfo| allow.allowance != Uint128::zero();
+    let allow_cmp = |l: &cw20::AllowanceInfo, r: &cw20::AllowanceInfo| l.spender.cmp(&r.spender);
+
+    let mut allowances: Vec<_> = allowances.into_iter().filter(nonzero_allow).collect();
+    allowances.sort_by(allow_cmp);
+
+    let mut result: cw20::AllAllowancesResponse = app
+        .wrap()
+        .query_wasm_smart(
+            addr.clone(),
+            &cw20_base::msg::QueryMsg::AllAllowances {
+                owner: owner.to_string(),
+                start_after: None,
+                limit: None,
+            },
+        )
+        .expect(&format!(
+            "Query for allowances for {} on {} failed",
+            owner, addr
+        ));
+
+    result.allowances = result
+        .allowances
+        .into_iter()
+        .filter(nonzero_allow)
+        .collect();
+    result.allowances.sort_by(allow_cmp);
+
+    assert_eq!(result, cw20::AllAllowancesResponse { allowances });
+}
+
+#[track_caller]
+// Helper function asserting proper balance on cw20 contract
+fn assert_balance(app: &App, addr: Addr, owner: Addr, balance: u128) {
+    let result: cw20::BalanceResponse = app
+        .wrap()
+        .query_wasm_smart(
+            addr.clone(),
+            &cw20_base::msg::QueryMsg::Balance {
+                address: owner.to_string(),
+            },
+        )
+        .expect(&format!(
+            "Query for balance for {} on {} failed",
+            owner, addr
+        ));
+
+    assert_eq!(
+        result,
+        cw20::BalanceResponse {
+            balance: Uint128::new(balance)
+        }
+    );
+}
+
+#[track_caller]
+// Helper function asserting proper balance of native token
+fn assert_native_balance(app: &App, denom: &str, owner: Addr, balance: u128) {
+    let result = app
+        .wrap()
+        .query_balance(owner.clone(), denom)
+        .expect(&format!("Query for balance of {} for {}", denom, owner));
+
+    assert_eq!(result, coin(balance, denom));
 }
 
 #[test]
@@ -146,4 +216,213 @@ fn setup_liquidity_pool() {
     assert_eq!(res.return_amount, Uint128::new(5));
 
     // TODO: actually perform swap and check value
+}
+
+#[test]
+// Simple swap scenario
+//
+// * Create (token, native token) pair
+// * Give allowance for pair
+// * Provide liquidity to pair
+// * Validate proper allowance on token as well as tokens posession
+// * Perform single swap of native tokens to tokens
+// * Verify proper amount of tokens (including 0.3% fee)
+// * Perform single swap of tokens to native tokens
+// * Verify proper amount of tokens (including 0.3% fee)
+// * Withdraw liquidity, all fees should be also added
+fn single_swap() {
+    let mut app = mock_app();
+
+    let cw20_id = app.store_code(contract_cw20());
+    let pair_id = app.store_code(contract_pair());
+
+    // Initialize actors
+    let lp = Addr::unchecked("liquidity-provider");
+    app.init_bank_balance(&lp, coins(2000, "btc")).unwrap();
+
+    let trader = Addr::unchecked("trader");
+    app.init_bank_balance(&trader, coins(1000, "btc")).unwrap();
+
+    let trader_recv = Addr::unchecked("trader-recv");
+
+    let cash = app
+        .instantiate_contract(
+            cw20_id,
+            lp.clone(),
+            &cw20_base::msg::InstantiateMsg {
+                name: "Cash Money".to_owned(),
+                symbol: "cash".to_owned(),
+                decimals: 2,
+                initial_balances: vec![Cw20Coin {
+                    address: lp.to_string(),
+                    amount: Uint128::new(6000),
+                }],
+                mint: None,
+            },
+            &[],
+            "Cash",
+        )
+        .unwrap();
+
+    let pair = app
+        .instantiate_contract(
+            pair_id,
+            lp.clone(),
+            &InstantiateMsg {
+                asset_infos: [
+                    AssetInfo::Native("btc".to_owned()),
+                    AssetInfo::Token(cash.clone()),
+                ],
+                token_code_id: cw20_id,
+            },
+            &[],
+            "Pair",
+        )
+        .unwrap();
+
+    let PairInfo {
+        liquidity_token: lt,
+        ..
+    } = app
+        .wrap()
+        .query_wasm_smart(pair.clone(), &QueryMsg::Pair {})
+        .unwrap();
+
+    // Provide allowance
+    app.execute_contract(
+        lp.clone(),
+        cash.clone(),
+        &cw20_base::msg::ExecuteMsg::IncreaseAllowance {
+            spender: pair.to_string(),
+            amount: Uint128::new(6000),
+            expires: None,
+        },
+        &[],
+    )
+    .unwrap();
+
+    // Provide liquidity
+    app.execute_contract(
+        lp.clone(),
+        pair.clone(),
+        &ExecuteMsg::ProvideLiquidity {
+            assets: [
+                Asset {
+                    info: AssetInfo::Native("btc".to_owned()),
+                    amount: Uint128::new(2000),
+                },
+                Asset {
+                    info: AssetInfo::Token(cash.clone()),
+                    amount: Uint128::new(6000),
+                },
+            ],
+            slippage_tolerance: None,
+        },
+        &coins(2000, "btc"),
+    )
+    .unwrap();
+
+    assert_native_balance(&app, "btc", lp.clone(), 0);
+    assert_native_balance(&app, "btc", trader.clone(), 1000);
+    assert_native_balance(&app, "btc", trader_recv.clone(), 0);
+    assert_native_balance(&app, "btc", pair.clone(), 2000);
+    assert_balance(&app, cash.clone(), lp.clone(), 0);
+    assert_balance(&app, cash.clone(), trader.clone(), 0);
+    assert_balance(&app, cash.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, cash.clone(), pair.clone(), 6000);
+    assert_balance(&app, lt.clone(), lp.clone(), 3464);
+    assert_balance(&app, lt.clone(), trader.clone(), 0);
+    assert_balance(&app, lt.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, lt.clone(), pair.clone(), 0);
+    assert_allowances(&app, cash.clone(), lp.clone(), vec![]);
+
+    // Swap btc for cash
+    app.execute_contract(
+        trader.clone(),
+        pair.clone(),
+        &ExecuteMsg::Swap {
+            offer_asset: Asset {
+                info: AssetInfo::Native("btc".to_owned()),
+                amount: Uint128::new(1000),
+            },
+            belief_price: None,
+            max_spread: None,
+            to: None,
+        },
+        &coins(1000, "btc"),
+    )
+    .unwrap();
+
+    assert_native_balance(&app, "btc", lp.clone(), 0);
+    assert_native_balance(&app, "btc", trader.clone(), 0);
+    assert_native_balance(&app, "btc", trader_recv.clone(), 0);
+    assert_native_balance(&app, "btc", pair.clone(), 3000);
+    assert_balance(&app, cash.clone(), lp.clone(), 0);
+    assert_balance(&app, cash.clone(), trader.clone(), 1994);
+    assert_balance(&app, cash.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, cash.clone(), pair.clone(), 4006);
+    assert_balance(&app, lt.clone(), lp.clone(), 3464);
+    assert_balance(&app, lt.clone(), trader.clone(), 0);
+    assert_balance(&app, lt.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, lt.clone(), pair.clone(), 0);
+    assert_allowances(&app, cash.clone(), lp.clone(), vec![]);
+
+    // Swap cash for btc
+    app.execute_contract(
+        trader.clone(),
+        cash.clone(),
+        &cw20_base::msg::ExecuteMsg::Send {
+            contract: pair.to_string(),
+            amount: Uint128::new(1000),
+            msg: to_binary(&Cw20HookMsg::Swap {
+                belief_price: None,
+                max_spread: None,
+                to: Some(trader_recv.to_string()),
+            })
+            .unwrap(),
+        },
+        &[],
+    )
+    .unwrap();
+
+    assert_native_balance(&app, "btc", lp.clone(), 0);
+    assert_native_balance(&app, "btc", trader.clone(), 0);
+    assert_native_balance(&app, "btc", trader_recv.clone(), 599);
+    assert_native_balance(&app, "btc", pair.clone(), 2401);
+    assert_balance(&app, cash.clone(), lp.clone(), 0);
+    assert_balance(&app, cash.clone(), trader.clone(), 994);
+    assert_balance(&app, cash.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, cash.clone(), pair.clone(), 5006);
+    assert_balance(&app, lt.clone(), lp.clone(), 3464);
+    assert_balance(&app, lt.clone(), trader.clone(), 0);
+    assert_balance(&app, lt.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, lt.clone(), pair.clone(), 0);
+    assert_allowances(&app, cash.clone(), lp.clone(), vec![]);
+
+    // Withdraw liqidity
+    app.execute_contract(
+        lp.clone(),
+        lt.clone(),
+        &cw20_base::msg::ExecuteMsg::Send {
+            contract: pair.to_string(),
+            amount: Uint128::new(3464),
+            msg: to_binary(&Cw20HookMsg::WithdrawLiquidity {}).unwrap(),
+        },
+        &[],
+    )
+    .unwrap();
+
+    assert_native_balance(&app, "btc", lp.clone(), 2401);
+    assert_native_balance(&app, "btc", trader.clone(), 0);
+    assert_native_balance(&app, "btc", trader_recv.clone(), 599);
+    assert_native_balance(&app, "btc", pair.clone(), 0);
+    assert_balance(&app, cash.clone(), lp.clone(), 5006);
+    assert_balance(&app, cash.clone(), trader.clone(), 994);
+    assert_balance(&app, cash.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, cash.clone(), pair.clone(), 0);
+    assert_balance(&app, lt.clone(), lp.clone(), 0);
+    assert_balance(&app, lt.clone(), trader.clone(), 0);
+    assert_balance(&app, lt.clone(), trader_recv.clone(), 0);
+    assert_balance(&app, lt.clone(), pair.clone(), 0);
+    assert_allowances(&app, cash.clone(), lp.clone(), vec![]);
 }
