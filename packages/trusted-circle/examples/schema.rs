4b9cf6608cd15b05c2fbd4a75632137b49a832b0
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-10-08 11:28:34 +0200

    Generate trusted-circle schema

--- packages/trusted-circle/examples/schema.rs
+++ packages/trusted-circle/examples/schema.rs
@@ -3,7 +3,10 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use trusted_circle::asset::{Asset, AssetInfo, PairInfo};
+pub use trusted_circle::{
+    DsoResponse, EscrowListResponse, EscrowResponse, ExecuteMsg, InstantiateMsg,
+    ProposalListResponse, ProposalResponse, QueryMsg, VoteListResponse, VoteResponse,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,7 +14,15 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(PairInfo), &out_dir);
-    export_schema(&schema_for!(Asset), &out_dir);
-    export_schema(&schema_for!(AssetInfo), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+
+    export_schema(&schema_for!(DsoResponse), &out_dir);
+    export_schema(&schema_for!(EscrowResponse), &out_dir);
+    export_schema(&schema_for!(EscrowListResponse), &out_dir);
+    export_schema(&schema_for!(ProposalResponse), &out_dir);
+    export_schema(&schema_for!(ProposalListResponse), &out_dir);
+    export_schema(&schema_for!(VoteResponse), &out_dir);
+    export_schema(&schema_for!(VoteListResponse), &out_dir);
 }
