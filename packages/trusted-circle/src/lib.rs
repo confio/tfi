a009d3f3885425874db971e0055cf9224a922255
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-10-27 18:51:58 +0200

    Cleanup unused code

--- packages/trusted-circle/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod msg;
-mod state;
-
-pub use msg::{
-    DsoResponse, Escrow, EscrowListResponse, EscrowResponse, ExecuteMsg, InstantiateMsg,
-    ProposalListResponse, ProposalResponse, QueryMsg, VoteInfo, VoteListResponse, VoteResponse,
-};
-pub use state::{EscrowStatus, PendingEscrow, ProposalContent, Votes, VotingRules};
